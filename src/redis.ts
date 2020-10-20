18711dd41f7546402319c075690e954a343db6a0
mgmtadmin <brandon.sorgdrager@gmail.com>
2020-10-19 21:13:51 -0500

    fix(redis): add password support

--- src/redis.ts
+++ src/redis.ts
@@ -1,3 +1,7 @@
 import Redis from "ioredis";
 
-export const redis = new Redis(process.env.REDISPORT ? parseInt(process.env.REDISPORT) : 6379, process.env.REDISHOST ? process.env.REDISHOST : "127.0.0.1");
+export const redis = new Redis({
+    port: process.env.REDISPORT ? parseInt(process.env.REDISPORT) : 6379, 
+    host: process.env.REDISHOST ? process.env.REDISHOST : "127.0.0.1",
+    password: process.env.REDISPASSWORD ? process.env.REDISPASSWORD : "127.0.0.1",
+})
