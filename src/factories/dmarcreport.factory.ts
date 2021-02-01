162b046d96accb9264c5e7e6525e5622e43d4768
root <root@out.rcvr.io>
2021-02-01 04:59:26 +0000

    feat(seed): add dmarc report seed

---
+++ src/factories/dmarcreport.factory.ts
@@ -0,0 +1,35 @@
+
+import Faker from 'faker'
+import { DmarcReport } from "../entity/DmarcReport";
+import { define } from 'typeorm-seeding'
+const generate = require('nanoid/generate')
+
+define(DmarcReport, (faker: typeof Faker) => {
+    var nowDate = new Date();
+    var pastDate = new Date();
+    pastDate.setDate(pastDate.getDate() - 15);
+    var domainName = faker.internet.domainName()
+    const dmarcReport = new DmarcReport()
+    var fqdnHost = faker.internet.domainWord() + '.' + domainName
+    var passFail = Math.random() < 0.5 ? 'pass' : 'fail';
+
+    dmarcReport.processedTime = faker.date.between(pastDate, nowDate);
+    dmarcReport.msgHash = 'fakenoref'
+    dmarcReport.clientId = generate("1234567890abcdef", 12)
+    dmarcReport.orgName = domainName;
+    dmarcReport.dmarcDomain = domainName;
+    dmarcReport.sourceIp = faker.internet.ip();
+    dmarcReport.provider = 'tbd'
+    dmarcReport.hostname = fqdnHost;
+    dmarcReport.sourceCount = faker.random.number()
+    dmarcReport.disposition = "none"
+    dmarcReport.dmarcSpf = passFail;
+    dmarcReport.dmarcDkim = passFail;
+    dmarcReport.headerFrom = domainName;
+    dmarcReport.dkimDomain = domainName;
+    dmarcReport.dkimResult = passFail;
+    dmarcReport.spfDomain = domainName;
+    dmarcReport.spfResult = passFail;
+
+    return dmarcReport
+  })
---
+++ src/seeds/create-dmarcReports.seed.ts
@@ -0,0 +1,9 @@
+// create-pets.seed.ts
+import { Factory, Seeder } from 'typeorm-seeding'
+import { DmarcReport } from '../entity/DmarcReport'
+
+export default class CreateUsers implements Seeder {
+    public async run(factory: Factory): Promise<any> {
+      await factory(DmarcReport)().createMany(90,{ clientId: '1234567890' })
+    }
+  }
