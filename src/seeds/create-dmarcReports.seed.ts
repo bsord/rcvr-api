2cc849571ddf551625ca06dbdad8b40886f2df4b
bsord <brandon.sorgdrager@gmail.com>
2021-02-07 08:06:33 +0000

    feat(seed): generate better seed data

--- src/seeds/create-dmarcReports.seed.ts
+++
@@ -1,9 +0,0 @@
-// create-pets.seed.ts
-import { Factory, Seeder } from 'typeorm-seeding'
-import { DmarcReport } from '../entity/DmarcReport'
-
-export default class CreateUsers implements Seeder {
-    public async run(factory: Factory): Promise<any> {
-      await factory(DmarcReport)().createMany(15,{ clientId: '1234567890' })
-    }
-  }
