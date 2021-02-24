a1688f9b6dbbd602a8e0208e494b39a1e20f41dc
bsord <brandon.sorgdrager@gmail.com>
2021-02-24 06:35:12 +0000

    feat(verification): auto sign in after email verify

--- src/modules/user/ConfirmUser.ts
+++ src/modules/user/ConfirmUser.ts
@@ -1,27 +1,29 @@
-import { Resolver, Mutation, Arg} from "type-graphql";
+import { Resolver, Mutation, Arg, Ctx} from "type-graphql";
 import { User } from "../../entity/User";
 import { redis } from "../../redis";
 import { confirmUserPrefix } from "../constants/redisPrefixes";
+import { RequestContext } from "../../types/RequestContext";
 
 @Resolver()
 export class ConfirmUserResolver {
-  @Mutation(() => Boolean)
+  @Mutation(() => User)
   async confirmUser(
-      @Arg("token") token: string
-  ): Promise<boolean> {
+      @Arg("token") token: string,
+      @Ctx() ctx: RequestContext
+  ): Promise<User|undefined> {
     
     //getID from redis via token
     const userId = await redis.get(confirmUserPrefix + token)
 
     //set confirmed true on user 
     if(!userId){
-        return false;
+        return undefined;
     }
 
     await User.update({id: userId}, {confirmed: true});
     await redis.del(confirmUserPrefix + token);
-    
-    return true;
+    ctx.req.session!.userId = userId
+    return User.findOne({where:{id: userId}})
   }
 
 }
